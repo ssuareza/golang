97f3b67948ea88cf50491cfb46c6b1f126115fca
ssuareza <sebastian@typeform.com>
2019-11-22 10:26:01 +0100

    Cleaning some code

--- kubernetes/main.go
+++ kubernetes/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -11,6 +12,27 @@ import (
 
 type k8s struct {
 	clientset kubernetes.Interface
+}
+
+func main() {
+	// init
+	k8s, err := connect()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// get version
+	k8s.version()
+
+	// get nodes
+	//k8s.nodes()
+
+	// get pods
+	//k8s.pods()
+
+	// get namespaces
+	//k8s.namespaces()
 }
 
 func connect() (*k8s, error) {
@@ -27,86 +49,44 @@ func connect() (*k8s, error) {
 	return &client, nil
 }
 
-func (o *k8s) getVersion() (string, error) {
+func (o *k8s) version() {
 	version, err := o.clientset.Discovery().ServerVersion()
 	if err != nil {
-		return "", err
+		log.Fatal(err)
 	}
 
-	return fmt.Sprintf("%s", version), nil
+	fmt.Println(version)
 }
 
-func (o *k8s) getNamespaces() {
+func (o *k8s) namespaces() {
 	namespaces, err := o.clientset.CoreV1().Namespaces().List(metav1.ListOptions{})
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error listing namespaces: %v", err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
-
-	// Print type
-	//fmt.Printf("%T\n", namespaces)
 
 	for _, namespace := range namespaces.Items {
 		fmt.Println(namespace.Name)
 	}
-
-	//return fmt.Sprintf("%s", namespaces), nil
 }
 
-func (o *k8s) getPods() {
+func (o *k8s) pods() {
 	pods, err := o.clientset.CoreV1().Pods("security").List(metav1.ListOptions{})
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error listing pods: %v", err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
 
 	for _, pod := range pods.Items {
 		fmt.Println(pod.Name)
 	}
-
-	//return fmt.Sprintf("%s", pods), nil
 }
 
-func (o *k8s) getNodes() {
+func (o *k8s) nodes() {
 	nodes, err := o.clientset.CoreV1().Nodes().List(metav1.ListOptions{})
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error listing nodes: %v", err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
 
 	for _, node := range nodes.Items {
 		fmt.Printf("Node: %s\n", node.Name)
 	}
-
-	//return fmt.Sprintf("%s", nodes), nil
-}
-
-func main() {
-	// init
-	k8s, err := connect()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	v, err := k8s.getVersion()
-	fmt.Println(*v)
-	// getVersion
-	/*v, err := k8s.getVersion()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(v)*/
-
-	// getNodes
-	//k8s.getNodes()
-
-	// getPods
-	//k8s.getPods()
-
-	// getNamespaces
-	//k8s.getNamespaces()
-
-	// getLogs
-	//k8s.getLogs()
 }
