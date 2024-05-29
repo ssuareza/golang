cb237a892f94772b0800a5a460a67b007f85ea20
Sebastian Suarez <ssuareza@pm.me>
2024-05-29 14:44:55 +0200

    Create wise app

---
+++ wise/README.md
@@ -0,0 +1,40 @@
+# wise
+
+A cli to use Wise API to obtain data of your account.
+
+## Usage
+
+1. Build application:
+
+```sh
+make build
+```
+
+2. Create configuration file on "$HOME/.config/wise/wise.yml":
+
+```yaml
+api_endpoint: https://api.wise.com
+api_key: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+profile_id: 12345678
+```
+
+3. And run:
+
+```sh
+build/wise
+```
+
+```text
+> Summary
+- Balance USD: 10000.20
+- Balance EUR: 8000.15
+- Rate USD to EUR: 0.9216
+
+> Card transactions
+- 2024-05: 300.64 EUR (T-shirt 128.17 EUR)
+- 2024-04: 1005 EUR (New bike 830.14 EUR)
+- 2024-03: 344 EUR (Market 127.06 EUR)
+- 2024-02: 217.75 EUR (Doctor 166 EUR)
+- 2024-01: 150.99 EUR (Market 117.74 EUR)
+- 2023-12: 166.51 EUR (Amazon 156.37 EUR)
+```
---
+++ wise/pkg/config/config.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	yaml "gopkg.in/yaml.v3"
+)
+
+// Config struct
+type Config struct {
+	ApiEndpoint string `yaml:"api_endpoint"`
+	ApiKey      string `yaml:"api_key"`
+	ProfileID   string `yaml:"profile_id"`
+}
+
+// getConfigFile returns the path to the config file
+func getConfigFile() string {
+	return fmt.Sprintf("%s/%s", os.Getenv("HOME"), ".config/wise/wise.yml")
+}
+
+// New returns a new configuration
+func New() (Config, error) {
+	var config Config
+
+	// get config file
+	configFile := getConfigFile()
+
+	// open file
+	file, err := os.Open(configFile)
+	if err != nil {
+		return config, err
+	}
+	defer file.Close()
+
+	// decode yaml
+	if file != nil {
+		decoder := yaml.NewDecoder(file)
+		if err := decoder.Decode(&config); err != nil {
+			return config, err
+		}
+	}
+
+	return config, nil
+}
